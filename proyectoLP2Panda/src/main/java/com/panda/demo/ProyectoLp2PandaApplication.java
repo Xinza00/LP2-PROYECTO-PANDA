248cd4b64c1f48ac061b8f6a341aa08358d14e19
RPantaX <pantajefferson173@gmail.com>
2024-06-25 21:44:41 -0500

    creacion proyecto base

---
+++ proyectoLP2Panda/src/main/java/com/panda/demo/ProyectoLp2PandaApplication.java
@@ -0,0 +1,13 @@
+package com.panda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLp2PandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLp2PandaApplication.class, args);
+	}
+
+}
